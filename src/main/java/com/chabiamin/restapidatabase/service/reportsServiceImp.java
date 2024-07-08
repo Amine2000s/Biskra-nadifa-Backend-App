ae2d0509d90cec0c37a41411f5c6d1b3bb822030
Amine2000s <92222814+Amine2000s@users.noreply.github.com>
2024-07-08 06:33:36 +0100

    fixed reportserviceimp

--- src/main/java/com/chabiamin/restapidatabase/service/reportsServiceImp.java
+++ src/main/java/com/chabiamin/restapidatabase/service/reportsServiceImp.java
@@ -1,16 +1,18 @@
 package com.chabiamin.restapidatabase.service;
 
 
-import com.chabiamin.restapidatabase.model.Report;
-import com.chabiamin.restapidatabase.repository.reportsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
 
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
+        import com.chabiamin.restapidatabase.model.Report;
+        import com.chabiamin.restapidatabase.repository.reportsRepository;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Component;
+        import org.springframework.web.multipart.MultipartFile;
+
+
+        import java.io.IOException;
+        import java.util.List;
+        import java.util.Optional;
 
 @Component
 public class reportsServiceImp implements reportsService  {
